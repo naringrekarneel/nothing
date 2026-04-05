e01b663656b8cbfba8b8b6b74472b21e8805eb77
Neel Naringrekar <naringrekarneel3@gmail.com>
2026-04-06 01:44:29 +0530

    Add files via upload

---
+++ kgtolbs.py
@@ -0,0 +1,19 @@
+n = input("KG / LBS :")
+w = float(input("ENTER THE WEIGHT :"))
+
+if n == "KG" or n == "kg":
+    print(w * 2.20462)   
+    
+elif n == "lbs" or n == "LBS":
+    print(w/2.205)
+"""naringrekarsu
+AIzaSyAPzJMQxQhR3Pkwhg-QbRBmT6VVHiVZHQw
+
+csk
+AIzaSyD9-S3gdevCrMqTWHiztMnyoob6QiP5OP8
+
+neel3
+AIzaSyCZ2Xt6HNESkcO95ddgv_uYnV3JoZwM878
+
+prn
+AIzaSyDths0j9f1d0t_p4bGmZrCZAAyAEVPBBSw"""
